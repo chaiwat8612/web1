e3b135f5605d2f78694e437f5a8bb8f94b0bcffc
chaiwat8612 <89202968+chaiwat8612@users.noreply.github.com>
2021-09-13 14:30:12 +0700

    Change example data

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -18,30 +18,24 @@ namespace WebApplication1.Controllers
             DataTable dt = new DataTable();
             dt.Clear();
 
-            dt.Columns.Add("umayNewsDoc",typeof(string));
-            dt.Columns.Add("typeModule", typeof(string));
-            dt.Columns.Add("title", typeof(string));
-            dt.Columns.Add("description", typeof(string));
-            dt.Columns.Add("metaTag", typeof(string));
-            dt.Columns.Add("metaDescription", typeof(string));
-            dt.Columns.Add("imageM", typeof(string));
-            dt.Columns.Add("imageL", typeof(string));
-            dt.Columns.Add("status", typeof(string));
-            dt.Columns.Add("startDate",typeof(DateTime));
-            dt.Columns.Add("endDate", typeof(DateTime));
-            dt.Columns.Add("createBy", typeof(string));
-            dt.Columns.Add("createDateTime", typeof(DateTime));
-            dt.Columns.Add("updateBy", typeof(string));
-            dt.Columns.Add("updateDateTime", typeof(DateTime));
-            dt.Columns.Add("newsDate", typeof(DateTime));
-            dt.Columns.Add("videoLink", typeof(string));
+            dt.Columns.Add("umayVideoId", typeof(string));
             dt.Columns.Add("category", typeof(string));
             dt.Columns.Add("priority", typeof(Int32));
+            dt.Columns.Add("typeModule", typeof(string));
+            dt.Columns.Add("title", typeof(string));
+            dt.Columns.Add("status", typeof(string));
+            dt.Columns.Add("startDate", typeof(DateTime));
+            dt.Columns.Add("endDate", typeof(DateTime));
+            dt.Columns.Add("videoDate", typeof(DateTime));
+            dt.Columns.Add("videoLink", typeof(string));
+            dt.Columns.Add("createBy", typeof(string));
+            dt.Columns.Add("createDate", typeof(DateTime));
+            dt.Columns.Add("updateBy", typeof(string));
+            dt.Columns.Add("updateDate", typeof(DateTime));
 
-            dt.Rows.Add(new object[] { "2021-060000022", "NEWS", "Umay+ พร้อมดูแลคุณ", "รายละเอียด โปรโมชัน", "700", "700", "2021-060000022M.jpg", "2021-060000022L.jpg", "Y", "2021-06-28 00:00:00", "2021-06-28 00:00:00", "ADMIN", "2021-06-28 09:49:22", "", "2021-06-28 09:49:22", "2021-06-28 09:49:00", "https://www.youtube.com/watch?v=tQ1wvUe6n8k", "promotion", 1});
-            dt.Rows.Add(new object[] { "2021-060000024", "NEWS", "บริการเพิ่มวงเงินผ่าน Umay+ Mobile Application", "รายละเอียด บริการ", "Increase", "Increase", "2021-060000024M.jpg", "2021-060000024L.jpg", "Y", "2021-08-18 00:00:00", "2021-09-18 00:00:00", "ADMIN", "2021-08-18 18:25:00", "", "2021-08-18 18:25:00", "2021-08-18 00:00:00", "https://www.youtube.com/watch?v=MoEYvVa14y4", "service", 1 });
-            dt.Rows.Add(new object[] { "2021-060000025", "NEWS", "รอยยิ้มของผู้ให้ คือรอยยิ้มที่มีค่ามากที่สุด", "รายละเอียด กิจกรรม", "Smile", "Smile", "2021-060000025M.jpg", "2021-060000025L.jpg", "Y", "2021-08-19 00:00:00", "2021-09-19 00:00:00", "ADMIN", "2021-08-19 18:25:00", "", "2021-08-19 18:25:00", "2021-08-19 00:00:00", "https://www.youtube.com/watch?v=24JOuKAdpio", "activity", 1 });
-            dt.Rows.Add(new object[] { "2021-060000026", "NEWS", "PAY IT FORWARD 9", "อื่นๆ", "Happy Sprite", "Happy Sprite", "2021-060000026M.jpg", "2021-060000026L.jpg", "Y", "2021-08-20 00:00:00", "2021-09-20 00:00:00", "ADMIN", "2021-08-20 18:25:00", "", "2021-08-20 18:25:00", "2021-08-20 00:00:00", "https://www.youtube.com/watch?v=RUlM8aJ8vZE", "", 1 });
+            dt.Rows.Add(new object[] { "2021-060000022", "PROMOTION", 0, "VIDEO", "Umay+ | เปิดฟลอร์ความสุขกับสไปร์ท บะบะบิ", "Y", "2021-08-24 00:00:00", "2031-09-24 00:00:00", "2020-11-21 00:00:00", "https://www.youtube.com/watch?v=WYv2BYvs_7Y", "ADMIN", "2021-08-30 00:00:00", "ADMIN", "2021-08-30 00:00:00" });
+            dt.Rows.Add(new object[] { "2021-060000024", "ACTIVITY", 0, "VIDEO", "Umay+ ให้น้องวางใจ...ง่ายทุกฝัน", "Y", "2021-08-24 00:00:00", "2031-09-24 00:00:00", "2020-09-20 00:00:00", "https://www.youtube.com/watch?v=Mqk5SVPq-_w", "ADMIN", "2021-08-30 00:00:00", "ADMIN", "2021-08-30 00:00:00" });
+            dt.Rows.Add(new object[] { "2021-060000024", "OTHER", 0, "VIDEO", "Umay+ เพราะรอยยิ้มของเรามาจากความใส่ใจ", "Y", "2021-08-24 00:00:00", "2031-09-24 00:00:00", "2020-05-22 09:49:22", "https://www.youtube.com/watch?v=1Iq32pXojRk", "ADMIN", "2021-08-30 00:00:00", "ADMIN", "2021-08-30 00:00:00" });
 
             DataSet DS = new DataSet();
 
