076a4b3442abffe6e760df4488130d167737af3b
chaiwat8612 <89202968+chaiwat8612@users.noreply.github.com>
2021-09-23 17:37:57 +0700

    Separate code to function

--- WebApplication1/wwwroot/js/video.js
+++ WebApplication1/wwwroot/js/video.js
@@ -2,56 +2,110 @@
 
     reportWindowSize();
 
-    function reportWindowSize() {
-        const heightOutput = document.querySelector('#height');
-        const widthOutput = document.querySelector('#width');
-        heightOutput.textContent = window.innerHeight;
-        widthOutput.textContent = window.innerWidth;
-
-        if (window.innerWidth < 400) {
-
-            $("#video0").hide();
-            $("#video1").hide();
-            $("#video2").hide();
-            $("#video3").hide();
-            $("#back_bn").show();
-            $("#next_bn").show();
-
-        } else {
-            $("#video0").show();
-            $("#video1").show();
-            $("#video2").show();
-            $("#video3").show();
-            $("#back_bn").hide();
-            $("#next_bn").hide();
-
-        }
-
-        if ($("#current").text() == 0) {
-            $("#video0").show();
-        } else if ($("#current").text() == 1) {
-            $("#video1").show();
-        } else if ($("#current").text() == 2) {
-            $("#video2").show();
-        } else if ($("#current").text() == 3) {
-            $("#video3").show();
-        }
-
-    }
-
     window.onresize = reportWindowSize;
+
+    document.getElementById("close_button").addEventListener("click", closeModal);
+    document.getElementById("myModal").addEventListener("click", closeModal);
 
 });
 
+function reportWindowSize() {
+
+    const heightOutput = document.querySelector('#height');
+    const widthOutput = document.querySelector('#width');
+    heightOutput.textContent = window.innerHeight;
+    widthOutput.textContent = window.innerWidth;
+
+    if (window.innerWidth < 400) {
+
+        hideAllVideo();
+        showButtonControl();
+        showCurrentVideo();
+
+    } else {
+
+        showAllVideo();
+        hideButtonControl();
+
+    }
+
+}
+
+function hideAllVideo() {
+
+    $("#video0").hide();
+    $("#video1").hide();
+    $("#video2").hide();
+    $("#video3").hide();
+
+}
+
+function showAllVideo() {
+
+    $("#video0").show();
+    $("#video1").show();
+    $("#video2").show();
+    $("#video3").show();
+
+}
+
+function hideButtonControl() {
+
+    $("#back_bn").hide();
+    $("#next_bn").hide();
+
+}
+
+function showButtonControl() {
+
+    $("#back_bn").show();
+    $("#next_bn").show();
+
+}
+
+function showCurrentVideo() {
+
+    if ($("#current").text() == 0) {
+        $("#video0").show();
+    } else if ($("#current").text() == 1) {
+        $("#video1").show();
+    } else if ($("#current").text() == 2) {
+        $("#video2").show();
+    } else if ($("#current").text() == 3) {
+        $("#video3").show();
+    }
+
+}
+
 function video_click(clicked_id) {
 
-    var tmp = document.getElementById(clicked_id).getAttribute("data-src");
+    // get modal element
+    var modal = document.getElementById("myModal");
 
-    var text_embed = "https://www.youtube.com/embed/" + tmp + "?autoplay=0&start=0";
+    //get video source
+    var video_souce = document.getElementById(clicked_id).getAttribute("data-src");
 
+    //create video source
+    var text_embed = "https://www.youtube.com/embed/" + video_souce + "?autoplay=0&start=0";
+
+    //set video source
     document.getElementById("embed_video").src = text_embed;
 
+    //show modal
     modal.style.display = "block";
+}
+
+function closeModal() {
+
+    // get modal element
+    var modal = document.getElementById("myModal");
+
+    //clear source and stop video
+    document.getElementById("embed_video").src = "";
+
+    //close modal
+    modal.style.display = "none";
+
 }
 
 $("#back_bn").click(function () {
@@ -109,32 +163,3 @@ $("#next_bn").click(function () {
         $("#current").text('0');
     }
 });
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function () {
-    document.getElementById("embed_video").src = "https://www.youtube.com/embed/nvq_lvC1MRY";
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    document.getElementById("embed_video").src = "";
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        document.getElementById("embed_video").src = "";
-        modal.style.display = "none";
-    }
-}
