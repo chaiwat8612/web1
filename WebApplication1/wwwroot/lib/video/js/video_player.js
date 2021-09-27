7c9a3f6842d7467d9f341c5c481bce771adc5f30
chaiwat8612 <89202968+chaiwat8612@users.noreply.github.com>
2021-09-27 17:20:40 +0700

    Implemented mobile vertical

--- WebApplication1/wwwroot/lib/video/js/video_player.js
+++ WebApplication1/wwwroot/lib/video/js/video_player.js
@@ -32,7 +32,28 @@ function appendYouTubeByID(videoID) {
     var loadSubscribe = document.getElementById("load-player");
 
     // create iframe
-    var youtube = '<iframe class="youtube-video" id="iframe_item" src="https://www.youtube.com/embed/' + videoID + '?autoplay=0&start=0" id="embed_video" allowscriptaccess="always" allow="autoplay" allowfullscreen="allowfullscreen"></iframe>';
+    var youtube = '';
+
+    if ($("#device_type").text() == "MOBILE") {
+
+        console.log("MOBILE");
+
+        youtube = '<iframe class="youtube-video" width="100%" height="540" style="border:none;" id="iframe_item" src="https://www.youtube.com/embed/';
+
+
+        $("#load-player").removeClass("embed-responsive");
+        $("#load-player").removeClass("embed-responsive-16by9");
+
+    } else {
+
+        youtube = '<iframe class="youtube-video" id="iframe_item" src="https://www.youtube.com/embed/'
+
+        $("#load-player").addClass("embed-responsive");
+        $("#load-player").addClass("embed-responsive-16by9");
+    }
+
+    // create iframe
+    youtube = youtube + videoID + '?autoplay=0&start=0" id="embed_video" allowscriptaccess="always" allow="autoplay" allowfullscreen="allowfullscreen"></iframe>';
 
     // add iframe
     loadSubscribe.innerHTML = youtube;
